841aa77cd35517e780852c20f4f94c4f2461c8ec
rubyferdianto <rubyferdianto@gmail.com>
2026-01-31 14:00:08 +0800

    update yaml

--- src/test/java/sg/edu/nus/iss/d13revision/controllers/PersonControllerTest.java
+++ src/test/java/sg/edu/nus/iss/d13revision/controllers/PersonControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
@@ -26,7 +26,7 @@ class PersonControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private PersonService personService;
 
     private List<Person> mockPersonList;
